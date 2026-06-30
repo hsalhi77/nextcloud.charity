f7e0443d649ad73fde9153783f639127d4ec211a
hsalhi77 <habib.salhi@yandex.com>
2026-06-30 18:40:41 +0400

    fix(charity): align LOV mapper table names with migration

--- lib/Db/cc_CaseTypeMapper.php
+++ lib/Db/cc_CaseTypeMapper.php
@@ -5,16 +5,17 @@ use OCP\IDBConnection;
 
 class cc_CaseTypeMapper extends CharityMapper {
     public function __construct(IDBConnection $db) {
-        parent::__construct($db, 'cc_caseType', cc_CaseType::class);
+        parent::__construct($db, 'cc_case_type', cc_CaseType::class);
     }
 
     public function find($id): ?cc_CaseType {
-        $sql = 'SELECT * FROM `*PREFIX*cc_caseType` WHERE `id` = ?';
+        $sql = 'SELECT * FROM `*PREFIX*cc_case_type` WHERE `id` = ?';
         return $this->findEntityString($sql, [$id]);
     }
 
     public function findAll() {
-        $sql = 'SELECT * FROM `*PREFIX*cc_caseType`';
+        $sql = 'SELECT * FROM `*PREFIX*cc_case_type`';
+
         return $this->findEntitiesString($sql, []);
     }
 }
--- lib/Db/cc_UpdateTypeMapper.php
+++ lib/Db/cc_UpdateTypeMapper.php
@@ -5,16 +5,17 @@ use OCP\IDBConnection;
 
 class cc_UpdateTypeMapper extends CharityMapper {
     public function __construct(IDBConnection $db) {
-        parent::__construct($db, 'cc_updateType', cc_UpdateType::class);
+        parent::__construct($db, 'cc_update_type', cc_UpdateType::class);
     }
 
     public function find($id): ?cc_UpdateType {
-        $sql = 'SELECT * FROM `*PREFIX*cc_updateType` WHERE `id` = ?';
+        $sql = 'SELECT * FROM `*PREFIX*cc_update_type` WHERE `id` = ?';
         return $this->findEntityString($sql, [$id]);
     }
 
     public function findAll() {
-        $sql = 'SELECT * FROM `*PREFIX*cc_updateType`';
+        $sql = 'SELECT * FROM `*PREFIX*cc_update_type`';
+
         return $this->findEntitiesString($sql, []);
     }
 }
